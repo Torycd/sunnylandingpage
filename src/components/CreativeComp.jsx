d2787b5357d7580c2e26b8a739a7ce68bcaca806
Torycd <vicidundun@gmail.com>
2024-03-24 12:11:31 +0100

    ll

--- src/components/CreativeComp.jsx
+++ src/components/CreativeComp.jsx
@@ -2,7 +2,7 @@
 const CreativeComp = () => {
   return (
     <div className="mt-[40px]">
-        <h1 className="text-5xl md:text-10xl font-bold text-center">We are creatives</h1>
+        <h1 className="text-5xl md:text-[5rem] font-bold text-center">We are creatives</h1>
         <img/>
     </div>
   )
